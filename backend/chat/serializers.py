f5af3a765a7c480578a931f6c34e705fca88852e
arunsarwesh <sarweshwarsarweshwar635@gmail.com>
2025-04-18 22:20:33 +0530

    Add profile_photo field to UserSerializer

--- backend/chat/serializers.py
+++ backend/chat/serializers.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "first_name", "last_name")
+        fields = ("id", "username", "first_name", "last_name", "profile_photo")
 
 class MessageSerializer(serializers.ModelSerializer):
     text = serializers.CharField(source="content")
