508f056af7c38568929478dd5b4f09b077eda268
arunsarwesh <sarweshwarsarweshwar635@gmail.com>
2025-04-22 15:20:26 +0530

    Enhance default admin user creation with additional fields for first name, last name, and role

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -11,6 +11,13 @@ class YourAppConfig(AppConfig):
         User = get_user_model()
         def create_default_admin(sender, **kwargs):
             if not User.objects.filter(email="alumni@karpagam.com").exists():
-                User.objects.create_superuser("Admin", "alumni@karpagam.com", "Admin@123.in")
-                # print("Default admin user created: admin / Admin@123.in")
+                User.objects.create_superuser(
+                    "Admin",
+                    "alumni@karpagam.com",
+                    "Admin@123.in",
+                    first_name="Gokul",
+                    last_name="MC",
+                    role="Admin"
+                )
+            # print("Default admin user created: admin / Admin@123.in")
         post_migrate.connect(create_default_admin, sender=self)
