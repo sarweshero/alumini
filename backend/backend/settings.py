aa3d2c607439975b7d9b5a8e3f0af55796b31ac3
arunsarwesh <sarweshwarsarweshwar635@gmail.com>
2025-05-11 21:58:34 +0530

    Update database password in settings.py for enhanced security

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Alumni',
         'USER': 'Alumni',
-        'PASSWORD': 'Alumni',
+        'PASSWORD': 'Sarwesh',
         'HOST': 'localhost',
         'PORT': '5432',
     }
