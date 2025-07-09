b6d0f84f1769dfd1ef99042eee3f37ff0437a634
sarweshero <sarweshero@gmail.com>
2025-07-09 14:03:19 +0530

    Add os import for MEDIA_ROOT path configuration in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
